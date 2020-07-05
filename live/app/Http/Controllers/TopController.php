e4cb03d043bb35224657c13ee513980c60246d4b
江南彪斗 <enamiayato@enamihyoutonoMacBook-Air.local>
2020-07-05 22:46:47 +0900

    [develop] create TopController

---
+++ live/app/Http/Controllers/TopController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TopController extends Controller
+{
+    // login
+    public function login()
+    {
+        return view('login');
+    }
+}
---
+++ live/resources/views/login.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>flower girls login</title>
+</head>
+<body>
+    <form action="/loginExec" method="POST">
+            player_id : <input type="text" name="player_id" size="40" placeholder="player_idを入力してください"> <br>
+        <input type="submit" value="Login">
+    </form>
+</body>
+</html>
