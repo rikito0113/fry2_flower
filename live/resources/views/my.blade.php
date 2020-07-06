9ca79948bcdb000f6dc474aa0074b6faeb658af8
nakamura.rikito <nakamura.rikito@Donuts-N608.local>
2020-07-06 14:35:58 +0900

    [create-top-blade]

---
+++ live/resources/views/my.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>flower girls</title>
+</head>
+<body>
+
+    トップ〜〜〜
+</body>
+</html>
