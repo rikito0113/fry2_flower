3b0b0d6317c346b3700cab8389e12d85932c2dfe
nakamura.rikito <nakamura.rikito@Donuts-N608.local>
2020-07-06 12:41:02 +0900

    [fix]hash

--- live/app/Http/Controllers/TopController.php
+++ live/app/Http/Controllers/TopController.php
@@ -9,12 +9,16 @@ class TopController extends Controller
     // login
     public function login()
     {
+        // ログイン処理execでhashをsessionに入れる
+        if (session()->has('hash')) {
+            return redirect()->route('top.loginExec');
+        }
         return view('login');
     }
 
     // login処理実行
     public function loginExec()
     {
-        
+
     }
 }
