7e8722340632bbd53d536a0015708304bcfbe388
江南彪斗 <enamiayato@enamihyoutonoMacBook-Air.local>
2020-07-07 02:32:58 +0900

    [develop] add presentcontroller

--- live/resources/views/header.blade.php
+++ live/resources/views/header.blade.php
@@ -1,7 +1,9 @@
 <div class="flower-header" >
-            <img class="header-child" src="{{ asset('/images/kaonashi.JPG') }}" alt="ロゴ">
-            <img class="header-child" src="{{ asset('/images/kaonashi.JPG') }}" alt="ロゴ">
-            <img class="header-child" src="{{ asset('/images/kaonashi.JPG') }}" alt="ロゴ">
-            <img class="header-child" src="{{ asset('/images/kaonashi.JPG') }}" alt="ロゴ">
-            <img class="header-child" src="{{ asset('/images/kaonashi.JPG') }}" alt="ロゴ">
+            <img class="header-child" src="{{ asset('/images/kaonashi.JPG') }}" alt="TOP">
+            <img class="header-child" src="{{ asset('/images/kaonashi.JPG') }}" alt="girl">
+            <img class="header-child" src="{{ asset('/images/kaonashi.JPG') }}" alt="gacha&shop">
+            <a href="{{ action('PresentController@index') }}">
+                <<img class="header-child" src="{{ asset('/images/kaonashi.JPG') }}" alt="present">
+            </a>
+            img class="header-child" src="{{ asset('/images/kaonashi.JPG') }}" alt="+">
 </div>
