7db451debc4d7ef08c873dc8f65328126d1e2ab1
nakamura.rikito <nakamura.rikito@Donuts-N608.local>
2020-07-06 14:41:33 +0900

    [route]

--- live/routes/web.php
+++ live/routes/web.php
@@ -14,4 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/',                            'TopController@login');
-Route::post('/loginExec',                   'TopController@loginExec');
+Route::post('/loginExec',                  'TopController@loginExec');
+Route::get('/my',                          'MyController@my');
