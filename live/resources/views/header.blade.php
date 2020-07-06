b9f9300a0ae0a83d76dc490605551baea60c4fff
江南彪斗 <enamiayato@enamihyoutonoMacBook-Air.local>
2020-07-06 23:57:17 +0900

    [develop] add header child

--- live/resources/views/header.blade.php
+++ live/resources/views/header.blade.php
@@ -1,3 +1,17 @@
-<div class="header" style="background-color:skyblue">
-    私はヘッダーだ
+<div class="flower-header" >
+    <div class="header-child">
+        TOP
+    </div>
+    <div class="header-child">
+        girl
+    </div>
+    <div class="header-child">
+        gacha&shop
+    </div>
+    <div class="header-child">
+        プレゼント
+    </div>
+    <div class="header-child">
+        ＋
+    </div>
 </div>
