185749fa9b9efc7edd78c4a932134989ea24b764
maaaaakoto35 <hajimeyo13@icloud.com>
2020-07-07 19:08:33 +0900

    [develop] fix girl_select

--- live/resources/views/girl_select.blade.php
+++ live/resources/views/girl_select.blade.php
@@ -10,11 +10,15 @@
         現在はパラメータでplayer_idを渡しているがJSで実装する方が良さそう
         https://qiita.com/horikeso/items/cceb42be04e2b6d1d5a6
     --}}
+    @include('header')
+
     <div style="text-align:center;">
-        @foreach ($charInfo as $char)
+        @foreach ($char_info as $char)
             <div>
                 <a href="/girl_select/{{ $player_id }}/{{ $char->char_id }}">{{ $char->name }}</a>
             </div><br>
         @endforeach
     </div>
+
+    @include('footer')
 </html>
