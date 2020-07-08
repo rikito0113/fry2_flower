4c3cfc1958a297ec6d51c9738df7645cf8b49d02
nakamura.rikito <nakamura.rikito@Donuts-N608.local>
2020-07-08 11:04:20 +0900

    test

--- live/app/Http/Controllers/Controller.php
+++ live/app/Http/Controllers/Controller.php
@@ -18,21 +18,22 @@ class Controller extends BaseController
 
     public function init()
     {
-        // ハッシュ値を保持している時
-        if (session()->has('hash')) {
-            $hash = session('hash');
-            $AllPlayer = Player::latest()->get();
-            foreach ($AllPlayer as &$player) {
-                if (Hash::check($hash, $player->player_id)) {
-                    $this->_playerId = $player->player_id;
-                }
-            }
-        }
+        // // ハッシュ値を保持している時
+        // if (session()->has('hash')) {
+        //     $hash = session('hash');
+        //     $AllPlayer = Player::latest()->get();
+        //     foreach ($AllPlayer as &$player) {
+        //         if (Hash::check($hash, $player->player_id)) {
+        //             $this->_playerId = $player->player_id;
+        //         }
+        //     }
+        // }
 
-        // もしplayerIdが取れなかった場合はloginへ
-        if (!$this->_playerId) {
-            session()->flush();
-            return redirect()->route('login');
-        }
+        // // もしplayerIdが取れなかった場合はloginへ
+        // if (!$this->_playerId) {
+        //     session()->flush();
+        //     return redirect()->route('login');
+        // }
+        $this->_playerId = 1;
     }
 }
