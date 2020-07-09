d38718989bfaf8f119a213d6e73bd65b6b2d6c9b
江南彪斗 <enamiayato@enamihyoutonoMacBook-Air.local>
2020-07-09 23:12:02 +0900

    [develop] girl select

--- live/resources/views/girl/index.blade.php
+++ live/resources/views/girl/index.blade.php
@@ -1,3 +1,14 @@
 @include('header')
-girlだ
+
+
+
+<div style="text-align:center;">
+    @foreach ($char_info as $char)
+        <div>
+            <a href="/girl_select/{{ $char->char_id }}">{{ $char->char_name }}</a>
+        </div><br>
+    @endforeach
+</div>
+
+
 @include('footer')
