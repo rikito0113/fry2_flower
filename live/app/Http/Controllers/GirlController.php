6dc5395a1aa425f32324ba70d01133291c28dbcc
nakamura.rikito <nakamura.rikito@Donuts-N608.local>
2020-07-14 12:07:04 +0900

    [wip]

--- live/app/Http/Controllers/GirlController.php
+++ live/app/Http/Controllers/GirlController.php
@@ -7,6 +7,7 @@ use App\CharacterData;
 use App\OwnedCharacterData;
 use App\Player;
 use App\OwnedCharacterImg;
+use App\PlayerChatLog;
 
 
 // ライブラリの呼び出し
@@ -81,6 +82,17 @@ class GirlController extends Controller
         $playerInfo = Player::where('player_id', $this->_playerId)->first();
         // 選択中のgirl情報
         $ownedCharInfo = GirlCore::girlLoad($playerInfo->owned_char_id);
+
+        $chatInstance = new PlayerChatLog;
+        $chatInstance->create([
+            'player_id'           => $this->_playerId,
+            'content'             => 'あああ',
+            'char_id'             => 1,
+            'char_avatar_id'      => 1,
+            'char_background_id'  => 1,
+            'is_player'           => 0,
+        ]);
+
         // プレイヤーの送った情報をinsert
         return view('girl.main-chat')
             ->with('owned_char_info',   $ownedCharInfo)
