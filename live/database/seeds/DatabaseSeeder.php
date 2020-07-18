94fd413692b8d18f395029ffaf867413ecc99165
江南彪斗 <enamiayato@enamihyoutonoMacBook-Air.local>
2020-07-18 20:48:50 +0900

    [develop] fix

--- live/database/seeds/DatabaseSeeder.php
+++ live/database/seeds/DatabaseSeeder.php
@@ -13,5 +13,9 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(CharacterDataTableSeeder::class);
         $this->call(CharacterImgTableSeeder::class);
+        $this->call(GirlTermSubjectTableSeeder::class);
+        $this->call(SubjectTableSeeder::class);
+        $this->call(TermTableSeeder::class);
+        $this->call(TitleTableSeeder::class);
     }
 }
