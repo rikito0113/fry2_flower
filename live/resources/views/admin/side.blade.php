61a376eb0685ce526f539c8721ac6c84e221ee11
nakamura.rikito <nakamura.rikito@Donuts-N608.elecom>
2020-07-19 15:57:12 +0900

    [admin]mennu

--- live/resources/views/admin/side.blade.php
+++ live/resources/views/admin/side.blade.php
@@ -1,9 +1,9 @@
 <div id="Header">
     <ul>
-        <li> <a href="{{ route('admin.sholdReply') }}" @if ($menu == 'shold_reply') class="active" @endif>未返信(未実装)</a></li>
-        <li><a href="{{ route('admin.findPlayer') }}" @if ($menu == 'find_player') class="active" @endif>プレイヤー検索(未実装)</a></li>
-        <li><a href="{{ route('admin.findItem') }}" @if ($menu == 'find_item') class="active" @endif>アイテム検索(未実装)</a></li>
-        <li><a href="{{ route('admin.findGirl') }}" @if ($menu == 'find_girl') class="active" @endif>ガール検索(未実装)</a></li>
-        <li><a href="{{ action('AdminController@registerTitle') }}" @if ($menu == 'regist_title') class="active" @endif>称号登録(未実装)</a></li>
+        <li> <a @if ($menu == 'shold_reply') class="active" @endif href="{{ route('admin.sholdReply') }}">未返信(未実装)</a></li>
+        <li><a @if ($menu == 'find_player') class="active" @endif href="{{ route('admin.findPlayer') }}">プレイヤー検索(未実装)</a></li>
+        <li><a @if ($menu == 'find_item') class="active" @endif href="{{ route('admin.findItem') }}">アイテム検索(未実装)</a></li>
+        <li><a @if ($menu == 'find_girl') class="active" @endif href="{{ route('admin.findGirl') }}">ガール検索(未実装)</a></li>
+        <li><a @if ($menu == 'regist_title') class="active" @endif href="{{ action('AdminController@registerTitle') }}">称号登録(未実装)</a></li>
     </ul>
 </div>
