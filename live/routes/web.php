3ecdedb3fcd3f75f1e551ca17a270dcc2c1e9c19
江南彪斗 <enamiayato@enamihyoutonoMacBook-Air.local>
2020-07-19 21:29:48 +0900

    [develop] fix#

--- live/routes/web.php
+++ live/routes/web.php
@@ -29,7 +29,7 @@ Route::get('/setImg/{imgId}',                            'GirlController@setImgE
 // profile関連  
 Route::get('/Profile/profile',                            'ProfileController@profile')->name('profile.profile');
 Route::post('/Profile/changeNameConfirm',                 'ProfileController@changeNameConfirm');
-Route::get('/Profile/changeNameExec/{$changeName}',       'ProfileController@changeNameExec');
+Route::get('/Profile/changeNameExec/{$changeName}',       'ProfileController@changeNameExec')->name('profile.changeNameExec');
 Route::post('/Profile/changeTitleConfirm',                'ProfileController@changeTitleConfirm')->name('profile.changeTitleConfirm');
 Route::get('/Profile/changeTitleExec/{$changeTitleId}',   'ProfileController@changeTitleExec')->name('profile.changeTitleExec');
 
