36a911a12074287a4ccf5e50b65a493522d6d5fa
江南彪斗 <enamiayato@enamihyoutonoMacBook-Air.local>
2020-07-22 00:07:26 +0900

    [develop] change seeder

--- live/database/seeds/GirlTermSubjectTableSeeder.php
+++ live/database/seeds/GirlTermSubjectTableSeeder.php
@@ -12,6 +12,8 @@ class GirlTermSubjectTableSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('girl_term_subject')->truncate();
+
         DB::table('girl_term_subject')->insert([
             [
                 'char_id'      => 1,
@@ -21,27 +23,27 @@ class GirlTermSubjectTableSeeder extends Seeder
             [
                 'char_id'      => 2,
                 'subject_id'   => 2,
-                'term_id'      => 2,
+                'term_id'      => 1,
             ],
             [
                 'char_id'      => 3,
                 'subject_id'   => 3,
-                'term_id'      => 3,
+                'term_id'      => 1,
             ],
             [
                 'char_id'      => 4,
                 'subject_id'   => 4,
-                'term_id'      => 4,
+                'term_id'      => 1,
             ],
             [
                 'char_id'      => 5,
                 'subject_id'   => 5,
-                'term_id'      => 5,
+                'term_id'      => 1,
             ],
             [
                 'char_id'      => 6,
                 'subject_id'   => 6,
-                'term_id'      => 6,
+                'term_id'      => 1,
             ],
         ]);
     }
