735e4c9c6cc1641e1f4f7a4748211276eef4a7c8
maaaaakoto35 <hajimeyo13@icloud.com>
2020-08-01 04:15:12 +0900

    [develop] fix findEventPlayer

--- live/app/Library/AdminCore.php
+++ live/app/Library/AdminCore.php
@@ -201,7 +201,7 @@ class AdminCore {
         if ($playerId) {
             foreach ($events as $key => $event) {
                 $eventPlayers[$key] = PlayerEventChatLog::where('player_id', $playerId)->where('scenario_id', $event->scenario_id)->first();
-                $eventPlayers[$key]->name = Player::where('player_id', $playerId)->first->pluck('name');
+                $eventPlayers[$key]->name = Player::where('player_id', $playerId)->first()->pluck('name');
             }
         } else {
             foreach ($events as $key => $event) {
