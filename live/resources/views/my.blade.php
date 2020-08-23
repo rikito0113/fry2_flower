4670a3ce1d2ec5d7fd57d3e3d8d47d2b4ff409af
nakamura.rikito <nakamura.rikito@Donuts-N608.elecom>
2020-08-24 01:48:51 +0900

    [index]css

--- live/resources/views/my.blade.php
+++ live/resources/views/my.blade.php
@@ -14,7 +14,7 @@
                 <div class="girl-img" style="width:30%; position:relative;">
                     <img src="{{ asset('/images/icon/bt_girl1.png') }}" alt="girl" width="100%"><br>
                     <img src="{{ asset('/images/button/bt_top_profile_pink.png') }}" alt="profile" style="position:absolute; bottom:30px; right:3px;" width="50%">
-                    <p style="position:absolute; bottom:25px; left:3px;">Lv {{$char->level}}</p>
+                    <p style="position:absolute; bottom:14px; left:3px; font-size: 6px; color: purple; font-weight: bold;">Lv {{$char->level}}</p>
                 </div>
                 @if ($index % 3 == 0)
                     <br>
