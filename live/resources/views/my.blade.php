1a8bd46be5f535af9c0a331a6253e6087b2d54e9
nakamura.rikito <nakamura.rikito@Donuts-N608.elecom>
2020-08-24 00:43:49 +0900

    [top]fix css

--- live/resources/views/my.blade.php
+++ live/resources/views/my.blade.php
@@ -4,27 +4,26 @@
     <div style="text-align:center;">
 
         {{-- イベント情報 スライダー表示 --}}
-        <div style="color: white; background-color: black; text-align:center;">イベント情報</div>
-        <div height="20%" style="text-align: center">
-            スライダーを表示する!!!!
+        <div style="text-align:center; width:100%">
+            <img src="{{ asset('/images/banner/bt_cam1.png') }}" alt="banner" width="100%"><br>
         </div>
-
-        <br><br>
-
+        <br>
         {{-- girl表示 --}}
-        @foreach ($owned_char_info as $index => $char)
-            <span width="20%">
-                <div class="girl-img" style="width:20%;">
-                    <img src="{{ asset('/images/character/'.$char->background_img.'.png') }}" alt="background" width="100%"><br>
-                    <div class="avatar">
-                        <img src="{{ asset('/images/character/'.$char->avatar_img.'.png') }}" alt="avatar" width="100%"><br>
+        <div class="top_girl_list">
+            @foreach ($owned_char_info as $index => $char)
+                <span width="30%">
+                    <div class="girl-img" style="width:30%;">
+                        <img src="{{ asset('/images/character/'.$char->background_img.'.png') }}" alt="background" width="100%"><br>
+                        <div class="avatar">
+                            <img src="{{ asset('/images/character/'.$char->avatar_img.'.png') }}" alt="avatar" width="100%"><br>
+                        </div>
                     </div>
-                </div>
-            </span>
-            @if ($index % 3 == 0)
-                <br>
-            @endif
-        @endforeach
+                </span>
+                @if ($index % 3 == 0)
+                    <br>
+                @endif
+            @endforeach
+        </div>
     </div>
 
     <br><br>
