9c3b81421399bab4585e95e4948782b95a0a1880
nakamura.rikito <nakamura.rikito@Donuts-N608.local>
2020-08-24 16:19:16 +0900

    [girl_top]css

--- live/resources/views/girl/status.blade.php
+++ live/resources/views/girl/status.blade.php
@@ -1,7 +1,7 @@
 @include('header')
 
 {{-- ガール情報 includeしてきてもいいかもしれない --}}
-<div style="position:relative; text-align:center; width:auto; height:50px; background-image: url('../images/bg/bg_header_dere.png'); background-size: contain">
+<div style="position:relative; text-align:center; width:auto; height:50px; background-image: url('../../images/bg/bg_header_dere.png'); background-size: contain">
     <img src="{{ asset('/images/bg/bg_header_clock_dere.png') }}" alt="bg_header_clock_dere" style="position:absolute; top:0px; left:0px;" width="45%" height="45px">
     <p style="position:absolute; top:-10px; left:5%; font-size: 12px; color: white; font-weight: bold;">{{ $current_date }}</p>
     <p style="position:absolute; bottom:-5px; left:44%; font-size: 15px; color: pink; font-weight: bold;">{{ $owned_char_info->char_name }}</p>
