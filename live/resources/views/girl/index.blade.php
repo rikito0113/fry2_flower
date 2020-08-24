95de14976b6f6d0f04b95f00fe450a6ba4227539
nakamura.rikito <nakamura.rikito@Donuts-N608.local>
2020-08-24 11:13:53 +0900

    [girl_top]fix ui

--- live/resources/views/girl/index.blade.php
+++ live/resources/views/girl/index.blade.php
@@ -2,8 +2,8 @@
 
 {{-- ガール情報 includeしてきてもいいかもしれない --}}
 <div style="position:relative; text-align:center; width:auto; height:50px; background-image: url('../images/bg/bg_header_tsun.png'); background-size: contain">
-    <img src="{{ asset('/images/bg/bg_header_clock_tsun.png') }}" alt="bg_header_clock_tsun" style="position:absolute; top:0px; left:5px;" width="40%" height="50px">
-    <p style="position:absolute; bottom:14px; left:100px; font-size: 6px; color: purple; font-weight: bold;">{{ $owned_char_info->char_name }}</p>
+    <img src="{{ asset('/images/bg/bg_header_clock_tsun.png') }}" alt="bg_header_clock_tsun" style="position:absolute; top:0px; left:0px;" width="45%" height="45px">
+    <p style="position:absolute; bottom:-5px; left:44%; font-size: 15px; color: purple; font-weight: bold;">{{ $owned_char_info->char_name }}</p>
 </div>
 
 <div style="text-align:center;">
