d9976fa153dccdd708703a601d282d9a0e8eb112
maaaaakoto35 <hajimeyo13@icloud.com>
2020-08-29 02:01:40 +0900

    [develop] fix memory path again

--- live/resources/views/girl/memory.blade.php
+++ live/resources/views/girl/memory.blade.php
@@ -4,7 +4,7 @@
 @include('girl.inc-girl-status')
 
 <div width="100%">
-    <img src="{{ asset('/images/titlebar/obi_cap_3.png') }}" alt="日常メモリー">
+    <img src="{{ asset('/images/titlebar/obi_cap3.png') }}" alt="日常メモリー">
     <div>2020年7月->夏</div>
     @for ($i = 0; $i < 12; $i++)
         @if ($i % 4 == 0)
@@ -13,10 +13,10 @@
 
         <span>
             @if ($i + 1 > $event_memory_length)
-                <img src="{{ asset('/images/button/bt_mem_place_seacret.png') }}" alt="シークレット">
+                <img src="{{ asset('/images/icon/bt_mem_place_seacret.png') }}" alt="シークレット">
                 <p class="mem_frame">?????</p>
             @else
-                <img src="{{ asset('/images/button/bt_mem_place'.$event_memory[$i]->scenario_id.'.png') }}" alt="開放済み">
+                <img src="{{ asset('/images/icon/bt_mem_place'.$event_memory[$i]->scenario_id.'.png') }}" alt="開放済み">
                 <p class="mem_frame">題名</p>
             @endif
         </span>
@@ -24,10 +24,10 @@
 </div>
 
 <div width="100%">
-    <img src="{{ asset('/images/titlebar/obi_cap_4.png') }}" alt="えっちなメモリー">
+    <img src="{{ asset('/images/titlebar/obi_cap4.png') }}" alt="えっちなメモリー">
     <div style="text-align: center">
-        <span style="padding-right: 10px"><img class="bt_main_mem_lv" src="{{ asset('/images/button/bt_event_h1.png') }}" alt="Lv達成"></span>
-        <span><img class="bt_main_mem_ev" src="{{ asset('/images/button/bt_event_h2.png') }}" alt="イベント"></span>
+        <span style="padding-right: 10px"><img class="bt_main_mem_lv" src="{{ asset('/images/button/bt_event_h1.png') }}" alt="Lv達成" width="30%"></span>
+        <span><img class="bt_main_mem_ev" src="{{ asset('/images/button/bt_event_h2.png') }}" alt="イベント" width="30%"></span>
     </div>
 
     <div class="wrapper_main_mem_lv">
