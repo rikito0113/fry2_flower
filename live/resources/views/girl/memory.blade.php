6a877c7077717dcdbef38f4258d2de9ae2cae0d1
maaaaakoto35 <hajimeyo13@icloud.com>
2020-08-29 02:07:40 +0900

    [develop] fix memory UI again

--- live/resources/views/girl/memory.blade.php
+++ live/resources/views/girl/memory.blade.php
@@ -4,7 +4,7 @@
 @include('girl.inc-girl-status')
 
 <div width="100%">
-    <img src="{{ asset('/images/titlebar/obi_cap3.png') }}" alt="日常メモリー">
+    <img src="{{ asset('/images/titlebar/obi_cap3.png') }}" alt="日常メモリー" width="100%">
     <div>2020年7月->夏</div>
     @for ($i = 0; $i < 12; $i++)
         @if ($i % 4 == 0)
@@ -13,10 +13,10 @@
 
         <span>
             @if ($i + 1 > $event_memory_length)
-                <img src="{{ asset('/images/icon/bt_mem_place_seacret.png') }}" alt="シークレット">
+                <img src="{{ asset('/images/icon/bt_mem_place_seacret.png') }}" alt="シークレット" width="29%">
                 <p class="mem_frame">?????</p>
             @else
-                <img src="{{ asset('/images/icon/bt_mem_place'.$event_memory[$i]->scenario_id.'.png') }}" alt="開放済み">
+                <img src="{{ asset('/images/icon/bt_mem_place'.$event_memory[$i]->scenario_id.'.png') }}" alt="開放済み" width="29%">
                 <p class="mem_frame">題名</p>
             @endif
         </span>
@@ -24,7 +24,7 @@
 </div>
 
 <div width="100%">
-    <img src="{{ asset('/images/titlebar/obi_cap4.png') }}" alt="えっちなメモリー">
+    <img src="{{ asset('/images/titlebar/obi_cap4.png') }}" alt="えっちなメモリー" width="100%">
     <div style="text-align: center">
         <span style="padding-right: 10px"><img class="bt_main_mem_lv" src="{{ asset('/images/button/bt_event_h1.png') }}" alt="Lv達成" width="30%"></span>
         <span><img class="bt_main_mem_ev" src="{{ asset('/images/button/bt_event_h2.png') }}" alt="イベント" width="30%"></span>
