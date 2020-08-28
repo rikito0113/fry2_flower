aea83794bf926f9bcfc93ddca0a559e101e7ee06
daigakuimo <enaena128@gmail.com>
2020-08-29 03:09:44 +0900

    [develop] fix

--- live/resources/views/study/girl_score_status.blade.php
+++ live/resources/views/study/girl_score_status.blade.php
@@ -10,7 +10,7 @@
         <div style="position:absolute; top:17%; left:17%;font-size:12px;">マサチューセッツ世界大学</div>
         <div style="position:absolute; top:43.5%; left:39%;">理系</div> 
 
-        <form action="/Study/upScoreExec" method="POST" style="position:absolute; width:40%; top:60%;">
+        <form action="/Study/upScoreExec" method="POST" style="position:absolute; top:60%; left:0; right:0; margin-left:auto; margin-left:auto;">
             @csrf
             <select name="add_score">
                 @for ($i = 0; $i <= $player_info->study_point; $i++)
@@ -18,7 +18,7 @@
                 @endfor
             </select>
             <input type="hidden" name="owned_char_id" value="{{$owned_girl_info->owned_char_id}}">
-            <input type="image" src="{{ asset('/images/button/bt_profile_mini1.png') }}" value="確定" style="position:absolute; width:40%; top:60%; margin-left:auto; margin-left:auto; left:0; right:0;">
+            <input type="image" src="{{ asset('/images/button/bt_profile_mini1.png') }}" value="確定" style="width:200%;">
         </form>
     </div>
 
