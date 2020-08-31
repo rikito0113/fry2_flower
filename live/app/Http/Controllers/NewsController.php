5ba81174229a450f0da17a1d1fda337108e72f05
nakamura.rikito <nakamura.rikito@Donuts-N608.elecom>
2020-08-31 21:19:14 +0900

    [news]sort

--- live/app/Http/Controllers/NewsController.php
+++ live/app/Http/Controllers/NewsController.php
@@ -12,43 +12,17 @@ class NewsController extends Controller
 {
     public function index()
     {
-        // $type = 1;
-        // $kidoku = NewsLog::where('player_id', $this->_playerId)->get();
-        // $news = News::all();
-        // $allNews = array();
-        // if (count($kidoku) != count($news)) {
-        //     // echo count($kidoku);
-        //     // echo count($news);
-        //     $type = 2;
-        // }
-        // if (isset($kidoku[0]) && $type == 2) {
-        //     foreach ($kidoku as $kRow) {
-        //         $kidokuTmp[] = $kRow['news_id'];
-        //     }
-        //     foreach ($news as $nRow) {
-        //         $newsTmp[] = $nRow['news_id'];
-        //     }
-        //     $allNewsId = array_diff($newsTmp, $kidokuTmp);
-        //     foreach ($allNewsId as $row) {
-        //         $allNews[] = News::where('news_id', $row)->first();
-        //     }
-        // } else {
-        //     $allNews = $news;
-        // }
-
-        // if (!isset($allNews[0])) {
-        //     $allNews = News::all();
-        // }
-
         $allNews = News::all();
         foreach($allNews as &$row) {
             $log = NewsLog::where('player_id', $this->_playerId)->where('news_id', $row['news_id'])->first();
+            $tmpNewsIds = array();
             if (isset($log)) {
                 $row['is_read'] = true;
+                $tmpNewsIds[] = $row['news_id'];
             }
         }
         // 逆順
-        $allNews = array_reverse($allNews);
+        array_multisort($tmpNewsIds, SORT_DESC, SORT_NUMERIC, $allNews);
 
         return view('news.index')
         ->with('all_news', $allNews);
