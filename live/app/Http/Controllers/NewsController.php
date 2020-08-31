ef27c6d059002932bfff89dcc402a51357a028c5
nakamura.rikito <nakamura.rikito@Donuts-N608.elecom>
2020-08-31 21:04:41 +0900

    [news]未読・既読

--- live/app/Http/Controllers/NewsController.php
+++ live/app/Http/Controllers/NewsController.php
@@ -12,37 +12,46 @@ class NewsController extends Controller
 {
     public function index()
     {
-        $type = 1;
-        $kidoku = NewsLog::where('player_id', $this->_playerId)->get();
-        $news = News::all();
-        $allNews = array();
-        if (count($kidoku) != count($news)) {
-            // echo count($kidoku);
-            // echo count($news);
-            $type = 2;
-        }
-        if (isset($kidoku[0]) && $type == 2) {
-            foreach ($kidoku as $kRow) {
-                $kidokuTmp[] = $kRow['news_id'];
-            }
-            foreach ($news as $nRow) {
-                $newsTmp[] = $nRow['news_id'];
-            }
-            $allNewsId = array_diff($newsTmp, $kidokuTmp);
-            foreach ($allNewsId as $row) {
-                $allNews[] = News::where('news_id', $row)->first();
-            }
-        } else {
-            $allNews = $news;
-        }
+        // $type = 1;
+        // $kidoku = NewsLog::where('player_id', $this->_playerId)->get();
+        // $news = News::all();
+        // $allNews = array();
+        // if (count($kidoku) != count($news)) {
+        //     // echo count($kidoku);
+        //     // echo count($news);
+        //     $type = 2;
+        // }
+        // if (isset($kidoku[0]) && $type == 2) {
+        //     foreach ($kidoku as $kRow) {
+        //         $kidokuTmp[] = $kRow['news_id'];
+        //     }
+        //     foreach ($news as $nRow) {
+        //         $newsTmp[] = $nRow['news_id'];
+        //     }
+        //     $allNewsId = array_diff($newsTmp, $kidokuTmp);
+        //     foreach ($allNewsId as $row) {
+        //         $allNews[] = News::where('news_id', $row)->first();
+        //     }
+        // } else {
+        //     $allNews = $news;
+        // }
 
-        if (!isset($allNews[0])) {
-            $allNews = News::all();
+        // if (!isset($allNews[0])) {
+        //     $allNews = News::all();
+        // }
+
+        $allNews = News::all();
+        foreach($allNews as &$row) {
+            $log = NewsLog::where('player_id', $this->_playerId)->where('news_id', $row['news_id'])->get();
+            if (isset($log)) {
+                $row['is_read'] = true;
+            } else {
+                $row['is_read'] = false;
+            }
         }
 
         return view('news.index')
-        ->with('all_news', $allNews)
-        ->with('type', $type);
+        ->with('all_news', $allNews);
     }
 
     // ツン・デレポイント/達成報酬
--- live/resources/views/news/index.blade.php
+++ live/resources/views/news/index.blade.php
@@ -1,14 +1,14 @@
 @include('header')
 
-@if ($type == 2)
-    未読あり<br>
-@else
-    既読一覧<br>
-@endif
-
 @if (isset($all_news))
     @foreach ($all_news as $row)
-        <a href="/News/detail/{{ $row->news_id }}">・{{ $row->title }}</a><br>
+        <a href="/News/detail/{{ $row->news_id }}">
+            ・
+            @if ($row->is_read != true)
+                [未読]
+            @endif
+            {{ $row->title }}
+        </a><br>
     @endforeach
 @endif
 
