c14c7a4f83c19da2262522992538b1690fef155e
nakamura.rikito <nakamura.rikito@Donuts-N608.elecom>
2020-09-03 22:45:07 +0900

    [js]slider

--- live/resources/views/header.blade.php
+++ live/resources/views/header.blade.php
@@ -6,6 +6,11 @@
     {{-- スライダー用 --}}
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.css">
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
+    <script type="module">
+        import Swiper from 'https://unpkg.com/swiper/swiper-bundle.esm.browser.min.js'
+
+        const swiper = new Swiper(...)
+    </script>
     <script src="https://unpkg.com/swiper/swiper-bundle.js"></script>
     <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
 
