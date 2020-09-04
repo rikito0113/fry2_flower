5b020610fb8603eb89b8652196c97ac0360f4c9a
daigakuimo <enaena128@gmail.com>
2020-09-04 13:00:40 +0900

    [develop] create study reward blade

--- live/resources/views/study/study_reward.blade.php
+++ live/resources/views/study/study_reward.blade.php
@@ -5,7 +5,7 @@
     <img src="{{ asset('/images/titlebar/obi_profile2.png') }}" alt="勉学の達成報酬" style="width:100%; vertical-align:top;"><br>
     <img src="{{ asset('/images/banner/bt_ranking' . $term->term_id . '.png') }}" alt="テスト期間" style="width:100%; vertical-align:top;"><br>
     <br>
-    <div width="90%">
+    <div style="margin:0 2%">
         <ul class="reward-tab">
             @foreach ($all_owned_char_info as $index => $char)
                 @if ($char->char_id == $owned_girl_info->char_id)
@@ -17,13 +17,13 @@
         </ul>
 
         <div style="text-align:center; position:relative; width:100%;">
-            <div style="position:absolute; height:auto">
-                <img src="{{ asset('/images/bg/bg_achieve_name' . $owned_girl_info->char_id . '.png') }}" alt="name">
-                <p style="position:absolute; top:25.5%; left:58%; font-size:30px;" class="study-font">{{ $owned_girl_info->score }}</p>
+            <div style="position:relative; height:auto; width:100%;">
+                <img src="{{ asset('/images/bg/bg_achieve_name' . $owned_girl_info->char_id . '.png') }}" alt="name" class="fit-img">
+                <p style="position:absolute; top:-27.5%; right:8%; font-size:30px;" class="study-font">{{ $owned_girl_info->score }}</p>
             </div>
             <div>
-                <img src="{{ asset('/images/bg/bg_achieve_chara' . $owned_girl_info->char_id . $owned_girl_info->attitude . '.png') }}" alt="chara_img">
-                <img src="{{ asset('/images/bg/bg_achieve_base.png') }}" alt="base">
+                <img src="{{ asset('/images/bg/bg_achieve_chara' . $owned_girl_info->char_id . '_' . $owned_girl_info->attitude . '.png') }}" alt="chara_img">
+                <img src="{{ asset('/images/bg/bg_achieve_base.jpg') }}" alt="base">
                 @foreach ($reward_list as $index => $reward)
                     @if ($reward->is_get)
                         <img src="{{ asset('/images/bg/bg_achieve_clear.png') }}" alt="clear">
@@ -39,7 +39,7 @@
                 @endforeach
             </div>
         </div>
-        <img src="{{ asset('/images/bg/bg_achieve_under' . $owned_girl_info->char_id . '.png') }}" alt="under">
+        <img src="{{ asset('/images/bg/bg_achieve_under' . $owned_girl_info->char_id . '.png') }}" alt="under" class="fit-img">
     </div>
 
     <br>
