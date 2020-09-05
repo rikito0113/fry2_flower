ef663fa13e87f16e5f0d2d8a8fb2dde2c55d0b98
nakamura.rikito <nakamura.rikito@Donuts-N608.elecom>
2020-09-05 17:20:17 +0900

    [news]detail css

--- live/resources/views/news/detail.blade.php
+++ live/resources/views/news/detail.blade.php
@@ -4,7 +4,7 @@
     <img src="{{ asset('/images/titlebar/obi_news1.png') }}" alt="obi_news1" width="100%"><br>
     <div class="bg-news">
         <img src="{{ asset('/images/bg/bg_news_read_upper.png') }}" alt="bg_news_read_upper" width="100%">
-        <p style="text-align: right;">{{ $news->created_at}}</p>
+        <div style="text-align: right; font-size:small;">{{ $news->created_at}}</div>
         <div class="news-title">
             title:: {{ $news->title}}<br>
         </div>
