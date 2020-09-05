b830940d394cef65c361ab64a3c28e2e65876551
nakamura.rikito <nakamura.rikito@Donuts-N608.elecom>
2020-09-05 16:51:28 +0900

    [news]detail

--- live/resources/views/news/detail.blade.php
+++ live/resources/views/news/detail.blade.php
@@ -2,8 +2,10 @@
 
 <div class="bg-pink-flower">
     <img src="{{ asset('/images/titlebar/obi_news1.png') }}" alt="obi_news1" width="100%"><br>
-    <img src="{{ asset('/images/bg/bg_news_read_upper.png') }}" alt="bg_news_read_upper" width="100%">
     <div class="bg-news">
+        <img src="{{ asset('/images/bg/bg_news_read_upper.png') }}" alt="bg_news_read_upper" width="100%">
+        <p style="text-align: right;">{{ $news->created_at}}</p>
+        <br>
         title:: {{ $news->title}}<br>
         content:: {!! $news->content !!}<br>
         aaaaaaaaaaaa<br>
@@ -12,8 +14,11 @@
         aaaaaaaaaaaa<br>
         aaaaaaaaaaaa<br>
         aaaaaaaaaaaa<br>
+        <br><br>
+        <a href="{{ action('NewsController@index') }}"><img src="{{ asset('/images/button/bt_news_back.png') }}" alt="bt_news_back" width="50%"></a>
+        <br><br>
+        <img src="{{ asset('/images/bg/bg_news_read_under.png') }}" alt="bg_news_read_under" width="100%">
     </div>
-    <img src="{{ asset('/images/bg/bg_news_read_under.png') }}" alt="bg_news_read_under" width="100%">
 </div>
 
 @include('footer')
