2758741d85e2aa9f424d77dfea6dd2d884911f74
nakamura.rikito <nakamura.rikito@Donuts-N608.elecom>
2020-09-05 16:32:35 +0900

    [news]detail

--- live/resources/views/news/detail.blade.php
+++ live/resources/views/news/detail.blade.php
@@ -1,6 +1,17 @@
 @include('header')
 
-title:: {{ $news->title}}<br>
-content:: {!! $news->content !!}<br>
+<div class="bg-pink-flower">
+    <img src="{{ asset('/images/titlebar/obi_news1.png') }}" alt="obi_news1" width="100%"><br>
+    <div class="bg-news">
+        title:: {{ $news->title}}<br>
+        content:: {!! $news->content !!}<br>
+        aaaaaaaaaaaa<br>
+        aaaaaaaaaaaa<br>
+        aaaaaaaaaaaa<br>
+        aaaaaaaaaaaa<br>
+        aaaaaaaaaaaa<br>
+        aaaaaaaaaaaa<br>
+    </div>
+</div>
 
 @include('footer')
