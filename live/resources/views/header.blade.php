8dd9f0239d46229df56db1a2ae07ac92e1c9c4c2
nakamura.rikito <nakamura.rikito@Donuts-N608.elecom>
2020-09-08 17:33:42 +0900

    [oauth]

--- live/resources/views/header.blade.php
+++ live/resources/views/header.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     {{-- スライダー用 --}}
     <link rel="stylesheet" href="//unpkg.com/swiper/swiper-bundle.min.css">
     <script src="//unpkg.com/swiper/swiper-bundle.min.js"></script>
--- live/resources/views/login.blade.php
+++ live/resources/views/login.blade.php
@@ -2,12 +2,13 @@
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>flower girls login</title>
 </head>
 <body>
     <form action="/loginExec" method="POST">
-        {{-- @csrf --}}
+        @csrf
         pf_player_id : <input type="text" name="pf_player_id" size="40" placeholder="pf_player_idを入力してください"> <br>
         <input type="submit" value="Login">
     </form>
