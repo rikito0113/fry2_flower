bcccf6eaf0b569b7cc17547d951e41db9fbd8247
nakamura.rikito <nakamura.rikito@Donuts-N608.elecom>
2020-09-08 16:40:04 +0900

    [oauth]test

--- live/app/Http/Kernel.php
+++ live/app/Http/Kernel.php
@@ -40,7 +40,8 @@ class Kernel extends HttpKernel
             // \Illuminate\Session\Middleware\StartSession::class,
             // \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            // \App\Http\Middleware\VerifyCsrfToken::class,
+            // sand_boxのエラー回避のために一旦削除 FIXME
+            \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
