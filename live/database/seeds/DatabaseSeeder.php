e2d2c7ef313799137db5057834e2054f8b72ddfc
maaaaakoto35 <hajimeyo13@icloud.com>
2020-09-09 22:25:09 +0900

    [develop] change event->event_info again

--- live/database/seeds/DatabaseSeeder.php
+++ live/database/seeds/DatabaseSeeder.php
@@ -25,6 +25,6 @@ class DatabaseSeeder extends Seeder
         $this->call(ItemTableSeeder::class);
         $this->call(ProloguePhraseSeeder::class);
         $this->call(StudyPointRewardTableSeeder::class);
-        $this->call(EventTableSeeder::class);
+        $this->call(EventInfoTableSeeder::class);
     }
 }
