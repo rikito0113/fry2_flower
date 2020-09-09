fd1b8a2236049cc13cc058c750c3030daf23fb63
nakamura.rikito <nakamura.rikito@Donuts-N608.elecom>
2020-09-10 01:24:28 +0900

    [title]

--- live/resources/views/index.blade.php
+++ live/resources/views/index.blade.php
@@ -1,9 +1,15 @@
-@include('header')
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>flower girls</title>
+</head>
+<body style="margin:0px;">
     <div>
-        <a href="{{ route('login') }}">始まるボタン</a>
+        <a href="{{ route('login') }}">
+            <img src="{{ asset('/images/title_logo.png') }}" alt="title_logo" width="100%">
+        </a>
     </div>
-
-    <div>
-        <a href="{{ route('register') }}">登録はこちら</a>
-    </div>
-@include('footer')
+</body>
+</html>
