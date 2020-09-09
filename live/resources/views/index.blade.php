3d010263d4fbac69dbc7eb90b7fe599f98eae13e
nakamura.rikito <nakamura.rikito@Donuts-N608.elecom>
2020-09-09 14:01:29 +0900

    [gadget]

--- live/resources/views/index.blade.php
+++ live/resources/views/index.blade.php
@@ -6,10 +6,4 @@
     <div>
         <a href="{{ route('register') }}">登録はこちら</a>
     </div>
-
-    <script>
-        nijiyome.auth({
-        state: 'd144c46760d8b0bc4c3e1a6143c744be',
-        });
-    </script>
 @include('footer')
