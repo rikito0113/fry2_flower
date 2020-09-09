bc9a94408bc17d53f05ae571147cbdb85d31d799
maaaaakoto35 <hajimeyo13@icloud.com>
2020-09-09 22:19:41 +0900

    [develop] change event->event_info

--- live/database/migrations/2020_09_09_195553_create_event_info_table.php
+++ live/database/migrations/2020_09_09_195553_create_event_info_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateEventTable extends Migration
+class CreateEventInfoTable extends Migration
 {
     /**
      * Run the migrations.
