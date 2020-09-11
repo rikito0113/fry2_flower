2b0ffdd90caa7af8d548ac5da63817272ca58f3e
maaaaakoto35 <hajimeyo13@icloud.com>
2020-09-11 23:31:05 +0900

    [develop] demo character_img seeder

--- live/database/seeds/CharacterImgTableSeeder.php
+++ live/database/seeds/CharacterImgTableSeeder.php
@@ -13,56 +13,24 @@ class CharacterImgTableSeeder extends Seeder
     public function run()
     {
         DB::table('character_img')->insert([
-            [
-                'char_id'           => 1,
-                'name'              => '齋藤飛鳥(制服)',
-                'category'          => 'avatar',
-            ],
-            [
-                'char_id'           => 2,
-                'name'              => '白石麻衣(制服)',
-                'category'          => 'avatar',
-            ],
-            [
-                'char_id'           => 3,
-                'name'              => '西野七瀬(制服)',
-                'category'          => 'avatar',
-            ],
-            [
-                'char_id'           => 4,
-                'name'              => '橋本奈々未(私服)',
-                'category'          => 'avatar',
-            ],
-            [
-                'char_id'           => 5,
-                'name'              => '生田絵梨花(制服)',
-                'category'          => 'avatar',
-            ],
-            [
-                'char_id'           => 6,
-                'name'              => '松村沙友理(制服)',
-                'category'          => 'avatar',
-            ],
-            [
-                'char_id'           => 1,
-                'name'              => '齋藤飛鳥(私服)',
-                'category'          => 'avatar',
-            ],
-            [
-                'char_id'           => 3,
-                'name'              => '西野七瀬(衣装)',
-                'category'          => 'avatar',
-            ],
-            [
-                'char_id'           => 4,
-                'name'              => '橋本奈々未(私服2)',
-                'category'          => 'avatar',
-            ],
-            [
-                'char_id'           => 5,
-                'name'              => '生田絵梨花(アップ)',
-                'category'          => 'avatar',
-            ],
+            ['char_id'=>1,'name'=>'花菱あおい','category'=>'bg','item_id'=>4011],
+            ['char_id'=>1,'name'=>'花菱あおい','category'=>'avatar','item_id'=>6001],
+            ['char_id'=>1,'name'=>'花菱あおい','category'=>'effect','item_id'=>7001],
+            ['char_id'=>2,'name'=>'橘花さくら','category'=>'bg','item_id'=>4014],
+            ['char_id'=>2,'name'=>'橘花さくら','category'=>'avatar','item_id'=>6002],
+            ['char_id'=>2,'name'=>'橘花さくら','category'=>'effect','item_id'=>7002],
+            ['char_id'=>3,'name'=>'桐島せら','category'=>'bg','item_id'=>4017],
+            ['char_id'=>3,'name'=>'桐島せら','category'=>'avatar','item_id'=>6003],
+            ['char_id'=>3,'name'=>'桐島せら','category'=>'effect','item_id'=>7003],
+            ['char_id'=>4,'name'=>'道明寺くるみ','category'=>'bg','item_id'=>4020],
+            ['char_id'=>4,'name'=>'道明寺くるみ','category'=>'avatar','item_id'=>6004],
+            ['char_id'=>4,'name'=>'道明寺くるみ','category'=>'effect','item_id'=>7004],
+            ['char_id'=>5,'name'=>'神代ゆめ','category'=>'bg','item_id'=>4023],
+            ['char_id'=>5,'name'=>'神代ゆめ','category'=>'avatar','item_id'=>6005],
+            ['char_id'=>5,'name'=>'神代ゆめ','category'=>'effect','item_id'=>7005],
+            ['char_id'=>6,'name'=>'椿姫まり','category'=>'bg','item_id'=>4026],
+            ['char_id'=>6,'name'=>'椿姫まり','category'=>'avatar','item_id'=>6006],
+            ['char_id'=>6,'name'=>'椿姫まり','category'=>'effect','item_id'=>7006],
         ]);
     }
 }
