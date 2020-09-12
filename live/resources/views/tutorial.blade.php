9e9daf300bb2b9e726b0825a3d3065ba81fc21c8
maaaaakoto35 <hajimeyo13@icloud.com>
2020-09-12 22:41:26 +0900

    [develop] add totorial at register

---
+++ live/resources/views/tutorial.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>flower girls login</title>
+</head>
+<body>
+    <div style="text-align: center;">チュートリアル</div>
+    <br><br>
+    <div style="text-align: center"><a href="{{ route('my.my') }}">スキップする</a></div>
+</html>
