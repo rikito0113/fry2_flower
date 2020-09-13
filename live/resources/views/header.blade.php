f15caf2ea88262c53773f0b001816bd51c2fe231
daigakuimo <enaena128@gmail.com>
2020-09-13 13:30:11 +0900

    [develop] fix slider

--- live/resources/views/header.blade.php
+++ live/resources/views/header.blade.php
@@ -18,6 +18,9 @@
     <script type="text/javascript" src="{{ asset('js/header.js') }}"></script>
 
     <script type="text/javascript" src="https://spgm.nijiyome.jp/js/touch.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
 
     <title>flower girls</title>
 </head>
