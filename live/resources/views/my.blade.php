723afbc7b458c45269ef6ed2e35cc7bd664b722d
daigakuimo <enaena128@gmail.com>
2020-09-13 10:48:10 +0900

    [develop] fix slider

--- live/resources/views/my.blade.php
+++ live/resources/views/my.blade.php
@@ -1,24 +1,25 @@
 @include('header')
 
     {{-- body --}}
-    <div class="container-fluid" style="text-align:center;">
+    <div class="container-fluid" style="text-align:center; padding:0px;">
 
         {{-- イベント情報 スライダー表示 --}}
         <div class="row">
             <div class="col-xs-12 col-sm-12 col-md-12" style="padding:1px;">
-                <!-- Additional required wrapper -->
-                <div class="swiper-wrapper">
-                    <!-- Slides -->
-                    <div class="swiper-slide"><img src="{{ asset('/images/banner/bn_top001.jpg') }}" alt="banner" style="width:100%;"></div>
-                    <div class="swiper-slide"><img src="{{ asset('/images/banner/bn_top002.jpg') }}" alt="banner" style="width:100%;"></div>
+                <div class="swiper-container">
+                    <!-- Additional required wrapper -->
+                    <div class="swiper-wrapper">
+                        <!-- Slides -->
+                        <div class="swiper-slide"><img src="{{ asset('/images/banner/bn_top001.jpg') }}" alt="banner" style="width:100%;"></div>
+                        <div class="swiper-slide"><img src="{{ asset('/images/banner/bn_top002.jpg') }}" alt="banner" style="width:100%;"></div>
+                    </div>
+                    <!-- If we need pagination -->
+                    <div class="swiper-pagination"></div>
+
+                    <!-- If we need navigation buttons -->
+                    <div class="swiper-button-prev"></div>
+                    <div class="swiper-button-next"></div>
                 </div>
-                <!-- If we need pagination -->
-                <div class="swiper-pagination"></div>
-
-                <!-- If we need navigation buttons -->
-                <div class="swiper-button-prev"></div>
-                <div class="swiper-button-next"></div>
-
             </div>
         </div>
 
