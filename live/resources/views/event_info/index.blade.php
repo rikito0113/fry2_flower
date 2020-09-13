00d87ac5e700602b2ba336b7859e8920b6aff73d
daigakuimo <enaena128@gmail.com>
2020-09-13 13:31:57 +0900

    [develop] fix slider

--- live/resources/views/event_info/index.blade.php
+++ live/resources/views/event_info/index.blade.php
@@ -5,7 +5,7 @@
 <div class="bg-pink-flower container-fluid" style="text-align:center; padding:0px;">
 <img src="{{ asset('/images/titlebar/obi_news1.png') }}" alt="obi_news1" class="fit-img100">
     {{-- イベント情報 スライダー表示 --}}
-    <div id="slider" class="carousel slide col-xs-12 col-sm-12 col-md-12" data-ride="carousel" style="padding:0px;">
+    <div id="slider-1" class="carousel slide" data-ride="carousel" style="padding:0px;">
         <div class="carousel-inner">
             <div class="carousel-item active">
                 <img src="{{ asset('/images/banner/bn_top001.jpg') }}" alt="banner" class="fit-img100">
@@ -13,15 +13,15 @@
             <div class="carousel-item">
                 <img src="{{ asset('/images/banner/bn_top002.jpg') }}" alt="banner" class="fit-img100">
             </div>
-            <a class="carousel-control-prev" href="#slider" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#slider" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
         </div>
+        <a class="carousel-control-prev" href="#slider-1" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#slider-1" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
     </div>
     <br>
 
