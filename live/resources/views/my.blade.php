eff1f71361489d29c91b9bfe37432fef575cb519
daigakuimo <enaena128@gmail.com>
2020-09-13 13:21:44 +0900

    [develop] fix slider

--- live/resources/views/my.blade.php
+++ live/resources/views/my.blade.php
@@ -12,15 +12,15 @@
                 <div class="carousel-item">
                     <img src="{{ asset('/images/banner/bn_top002.jpg') }}" alt="banner" class="fit-img100">
                 </div>
-                <a class="carousel-control-prev" href="#slider-1" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#slider-1" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
             </div>
+            <a class="carousel-control-prev" href="#slider-1" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#slider-1" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
         </div>
 
         <br>
