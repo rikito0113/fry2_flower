cd818d48e731a225ea29fac434223eb453c99e56
daigakuimo <enaena128@gmail.com>
2020-09-14 22:13:42 +0900

    [develop] fix girl info

--- live/resources/views/girl/inc-girl-status.blade.php
+++ live/resources/views/girl/inc-girl-status.blade.php
@@ -7,7 +7,7 @@
     <p class="girl-status-name">{{ $owned_char_info->char_name }}</p>
     <p class="girl-status-name-purple">{{ $owned_char_info->char_name }}</p>
     {{-- ステータスへ --}}
-    <div class="col-2 col-sm-2 col-md-2 offset-4 offset-sm-4 offset-md-4" style="padding:0px;">
+    <div class="col-2 col-sm-2 col-md-2 offset-4 offset-sm-4 offset-md-4" style="padding:7px;">
         <a href="/Girl/status/{{ $owned_char_info->owned_char_id }}"><img src="{{ asset('/images/icon/icon_status.png') }}" alt="bg_header_clock_dere" class="fit-img100"></a>
     </div>
 </div>
