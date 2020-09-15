b54c7123ae89d5a69cd29b04da637f56ffb2211b
nakamura.rikito <nakamura.rikito@Donuts-N608.elecom>
2020-09-15 12:14:05 +0900

    [api]

--- live/app/Library/ShopCore.php
+++ live/app/Library/ShopCore.php
@@ -13,14 +13,17 @@ class ShopCore
 {
     public static function buyItem($playerInfo, $itemId) {
         $url = "https://api.nijiyome.jp/api/rest/payment/@me/@self/@app";
-        $params = ['callbackUrl' => "https://flower-dev.maaaaakoto35.com/Shop/callback",
-                   'finishPageUrl' => "https://flower-dev.maaaaakoto35.com/Shop/index",
-                   'paymentItems' => ["itemId" => 1, "itemName" => "test", "unitPrice" => 100, "quantity" => 1, "imageUrl" => "http://example.com/ex101.jpg", "description" => "test"]];
+        $params =  ['paymentId' => 1,
+                    'appId' => 785,
+                    'userid' => $playerInfo->player_id,
+                    'callbackUrl' => "https://flower-dev.maaaaakoto35.com/Shop/callback",
+                    'finishPageUrl' => "https://flower-dev.maaaaakoto35.com/Shop/index",
+                    'paymentItems' => ["itemId" => 1, "itemName" => "test", "unitPrice" => 100, "quantity" => 1, "imageUrl" => "https://flower-dev.maaaaakoto35.com/ex101.jpg", "description" => "test"]];
         $client = new Client();
         $response = $client->request(
             'POST',
             $url, // URLを設定
-            ['query' => $params] // パラメーターがあれば設定 ?foo=barの場合は $params = ['foo' => 'bar']
+            ['headers' => ['Content-Type' => 'application/json'], 'query' => $params] // パラメーターがあれば設定 ?foo=barの場合は $params = ['foo' => 'bar']
         );
         echo $response->getStatusCode();
         echo $response->getReasonPhrase(); // OK
