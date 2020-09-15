0d9ba3bbedcd8e702fa12f77c01c522aa9e151f7
nakamura.rikito <nakamura.rikito@Donuts-N608.elecom>
2020-09-15 17:47:53 +0900

    [api]

--- live/resources/views/index.blade.php
+++ live/resources/views/index.blade.php
@@ -5,6 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script type="text/javascript" src="https://spgm.nijiyome.jp/js/touch.js"></script>
     <title>flower girls</title>
+    <script type="text/javascript" src="https://spgm.nijiyome.jp/js/touch.js"></script>
+    <script>
+        nijiyome.auth({
+        state: 'd144c46760d8b0bc4c3e1a6143c744be',
+        });
+    </script>
 </head>
 <body style="margin:0px;">
     <div>
