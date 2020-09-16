1746a315ddc302a1142fa8880309b0003e9c462e
daigakuimo <enaena128@gmail.com>
2020-09-16 22:44:32 +0900

    [develop] fix ui

--- live/resources/views/girl/inc-girl-status.blade.php
+++ live/resources/views/girl/inc-girl-status.blade.php
@@ -3,11 +3,11 @@
     <div class="col-6 col-sm-6 col-md-6" style="padding:0px;">
         <img src="{{ asset('/images/bg/bg_header_clock_dere.png') }}" alt="bg_header_clock_dere" class="fit-img100">
         <p class="girl-status-date">{{ $current_date }}</p>
-        <p class="girl-status-name">{{ $owned_char_info->char_name }}</p>
-        <p class="girl-status-name-purple">{{ $owned_char_info->char_name }}</p>
     </div>
     {{-- ステータスへ --}}
     <div class="col-2 col-sm-2 col-md-2 offset-4 offset-sm-4 offset-md-4" style="padding:7px;">
         <a href="/Girl/status/{{ $owned_char_info->owned_char_id }}"><img src="{{ asset('/images/icon/icon_status.png') }}" alt="bg_header_clock_dere" class="fit-img100"></a>
+        <p class="girl-status-name">{{ $owned_char_info->char_name }}</p>
+        <p class="girl-status-name-purple">{{ $owned_char_info->char_name }}</p>
     </div>
 </div>
