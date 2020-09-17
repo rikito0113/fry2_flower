c8f9574babf9c9316cddac2af689c40c36053f06
nakamura.rikito <nakamura.rikito@Donuts-N608.elecom>
2020-09-17 23:07:40 +0900

    [shop]

--- live/app/Library/ShopCore.php
+++ live/app/Library/ShopCore.php
@@ -16,18 +16,16 @@ class ShopCore
         $params =  ['callbackUrl' => "https://flower-dev.maaaaakoto35.com/Shop/callback",
                     'finishPageUrl' => "https://flower-dev.maaaaakoto35.com/Shop/index",
                     'paymentItems' => ["itemId" => 1, "itemName" => "test", "unitPrice" => 100, "quantity" => 1, "imageUrl" => "https://flower-dev.maaaaakoto35.com/ex101.jpg", "description" => "test"]];
-        $client = new Client();
-        $response = $client->request(
-            'POST',
-            $url, // URLを設定
-            ['headers' => ['Content-Type' => 'application/json'], 'query' => $params],
-        );
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_POST, TRUE);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $params); // パラメータをセット
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($curl);
+        $json = json_decode($response, true);
         echo '決済処理用:';
-        echo $response->getStatusCode();   // 200が正解?
-        echo $response->getReasonPhrase(); // OKが正解
 
-        $responseBody = $response->getBody()->getContents();
-        echo $responseBody;
+
     }
 
 }
