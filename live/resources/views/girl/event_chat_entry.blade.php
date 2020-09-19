70e87f1686b9b644b7e45da8dd301ea013ead4cb
maaaaakoto35 <hajimeyo13@icloud.com>
2020-09-19 22:30:08 +0900

    [develop] fix event_entry for field

--- live/resources/views/girl/event_chat_entry.blade.php
+++ live/resources/views/girl/event_chat_entry.blade.php
@@ -1,28 +1,36 @@
 
 @include('header')
 
-<div class="container-fluid" style="padding:0px 0px; text-align:center;">
+<div class="container-fluid bg-pink-flower" style="text-align:center;">
 
     {{-- Fieldがある時 --}}
     @if ($field_list)
         @foreach ($field_list as $key => $field)
             <div class="row">
                 @if ($field == '華姫寮')
-                    <a href="/Girl/eventPlace/華姫寮">
-                        <img src="{{ asset('/images/button/bt_place1.png') }}" alt="">
-                    </a>
+                    <div class="col-sm-10">
+                        <a href="/Girl/eventPlace/華姫寮">
+                            <img class="img-fluid" src="{{ asset('/images/button/bt_place1.png') }}" alt="">
+                        </a>
+                    </div>
                 @elseif ($field == '繁華街')
-                    <a href="/Girl/eventPlace/繁華街">
-                        <img src="{{ asset('/images/button/bt_place2.png') }}" alt="">
-                    </a>
+                    <div class="col-sm-10">
+                        <a href="/Girl/eventPlace/繁華街">
+                            <img class="img-fluid" src="{{ asset('/images/button/bt_place2.png') }}" alt="">
+                        </a>
+                    </div>
                 @elseif ($field == '学校')
+                <div class="col-sm-10">
                     <a href="/Girl/eventPlace/学校">
-                        <img src="{{ asset('/images/button/bt_place3.png') }}" alt="">
+                        <img class="img-fluid" src="{{ asset('/images/button/bt_place3.png') }}" alt="">
                     </a>
+                </div>
                 @elseif ($field == '出身地')
+                <div class="col-sm-10">
                     <a href="/Girl/eventPlace/出身地">
-                        <img src="{{ asset('/images/button/bt_place4.png') }}" alt="">
+                        <img class="img-fluid" src="{{ asset('/images/button/bt_place4.png') }}" alt="">
                     </a>
+                </div>
                 @endif
             </div><br>
         @endforeach
